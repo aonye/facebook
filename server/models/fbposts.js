5e7d77a2db22e27d1d64f704063181d17fd28f9b
Andy Ye <ye.andyy@gmail.com>
2022-11-03 11:47:08 -0700

    initial commit

--- server/models/fbposts.js
+++ server/models/fbposts.js
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
-	author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
-	title: { type: String, required: true, minlength: 1, maxlength: 30 },
-	timestamp: { type: Date, required: true },
-	post: { type: String, required: true, minlength: 1, maxlength: 10000 },
-	published: { type: Boolean, required: true },
-	likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
-	comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-});
+// const PostSchema = new Schema({
+// 	author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+// 	title: { type: String, required: true, minlength: 1, maxlength: 30 },
+// 	timestamp: { type: Date, required: true },
+// 	post: { type: String, required: true, minlength: 1, maxlength: 10000 },
+// 	published: { type: Boolean, required: true },
+// 	likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+// 	comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+// });
 
-export default mongoose.model("Post", PostSchema);
+// export default mongoose.model("Post", PostSchema);
